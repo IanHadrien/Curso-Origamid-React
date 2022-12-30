aa828a2abcfda1d906d2f89cee100e0f207a65f7
Ian Hadrien <ianhadrien@gmail.com>
2022-12-29 21:53:19 -0300

    Edição do App

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -1,9 +1,16 @@
+import { UseEffect } from "./aulas/useEffect";
+import { Desafio2useState } from "./desafios/desafio2-useState";
+import { Desafio3useEffect } from "./desafios/desafio3-useEffect";
 import { AppHooks } from "./hooks/AppHooks";
 
 const App = () => {
   return (
     <>
-      <AppHooks />
+      {/* <AppHooks /> */}
+      {/* <Desafio2useState /> */}
+
+      {/* <UseEffect /> */}
+      <Desafio3useEffect />
     </>
   );
 }
--- primeiro-app/src/hooks/AppHooks.js
+++ primeiro-app/src/hooks/AppHooks.js
@@ -4,11 +4,24 @@ import { Modal } from './Modal';
 
 export const AppHooks = () => {
   const [modal, setModal] = useState(false);
+  const [contar, setContar] = useState(1);
+  const [items, setItems] = useState(['Item 1']);
+
+  const handClick = () => {
+    setContar(contar + 1);
+    setItems([...items, 'Item ' + (contar + 1)]);
+  };
 
   return (
     <>
       <Modal modal={modal} setModal={setModal}/>
       <ButtonModal setModal={setModal}/>
+
+      <h2>useState pt2</h2>
+      {items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+      <button onClick={handClick}>{contar}</button>
     </>
   )
 }
